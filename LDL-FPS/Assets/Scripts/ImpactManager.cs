4b86f69b156a9099b613e744ecec045756a785f8
FEGYVEMB <fegyveres.miklos@gmail.com>
2023-08-09 16:18:05 +0200

    set up placeholder object and script for Impact Manager - create ImpactManager object with ImpactManager.cs script attached

---
+++ LDL-FPS/Assets/Scripts/ImpactManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpactManager : MonoBehaviour
+{
+    
+    Dictionary<string, GameObject> impactParticles = new Dictionary<string, GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
